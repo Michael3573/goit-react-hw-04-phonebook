821b7cdc4a220018f01f84349bde6417ed70d546
Michael3573 <126564353+Michael3573@users.noreply.github.com>
2023-10-15 14:47:54 +0200

    commit-04

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,51 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styles from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = e => {
+    setName(e.target.value);
   };
 
-  handleNameChange = e => {
-    this.setState({ name: e.target.value });
+  const handleNumberChange = e => {
+    setNumber(e.target.value);
   };
 
-  handleNumberChange = e => {
-    this.setState({ number: e.target.value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state.name, this.state.number);
-    this.setState({ name: '', number: '' });
+    onSubmit(name, number);
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          className={styles.form}
-          type="text"
-          name="name"
-          placeholder="   Name"
-          value={this.state.name}
-          onChange={this.handleNameChange}
-        />
-        <input
-          className={styles.form}
-          type="tel"
-          name="number"
-          placeholder="   Phone Number"
-          value={this.state.number}
-          onChange={this.handleNumberChange}
-        />
-        <button className={styles['add-btn']} type="submit">
-          Add Contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className={styles.form}
+        type="text"
+        name="name"
+        placeholder="   Name"
+        value={name}
+        onChange={handleNameChange}
+      />
+      <input
+        className={styles.form}
+        type="tel"
+        name="number"
+        placeholder="   Phone Number"
+        value={number}
+        onChange={handleNumberChange}
+      />
+      <button className={styles['add-btn']} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
 }
 
 export default ContactForm;
